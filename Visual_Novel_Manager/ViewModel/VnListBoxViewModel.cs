d2225d338258e34a0845b89b077e53559d59e723
Micah <Micah>
2016-06-18 20:59:23 -0700

    Setting up basic structure for the VisualNovelListBox

---
+++ Visual_Novel_Manager/ViewModel/VnListBoxViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Visual_Novel_Manager.Model;
+
+namespace Visual_Novel_Manager.ViewModel
+{
+    public class VnListBoxViewModel: INotifyPropertyChanged
+    {
+        #region property changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region constructor and setup
+        private VnListboxModel _listboxModel;
+        public VnListboxModel VnListboxModel
+        {
+            get { return _listboxModel; }
+            set
+            {
+                _listboxModel = value;
+                RaisePropertyChanged("VnListboxModel");
+            }
+        }
+
+        public VnListBoxViewModel()
+        {
+            _listboxModel = new VnListboxModel();
+        }
+        #endregion
+
+
+
+
+
+
+
+
+
+        private void SetVnId()
+        {
+            
+        }
+
+
+    }
+}
