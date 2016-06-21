19d419fe9afe2cd17225a3eb4bd2da398993496d
micah686 <legendofzeldafan3@yahoo.com>
2016-06-20 23:23:27 -0700

    Set up skeleton for Character tab

---
+++ Visual_Novel_Manager/ViewModel/CharacterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Visual_Novel_Manager.Model;
+
+namespace Visual_Novel_Manager.ViewModel
+{
+    public class CharacterViewModel: INotifyPropertyChanged
+    {
+        #region property changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        private CharacterModel _characterModel;
+
+        public CharacterModel CharacterModel
+        {
+            get { return _characterModel; }
+            set
+            {
+                _characterModel = value;
+                RaisePropertyChanged("CharacterModel");
+            }
+        }
+
+        public CharacterViewModel()
+        {
+            _characterModel = new CharacterModel();
+        }
+    }
+}
